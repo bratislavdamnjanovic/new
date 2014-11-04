04dc17b48e386e33d45da4b22b9f5382a1f7d6a5
ikakuca <ikakuca@gmail.com>
2014-11-04 19:00:36 +0100

    aa

--- index.php
+++ index.php
@@ -13,19 +13,38 @@ and open the template in the editor.
         <title>Remind me</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width">
-        <link rel="css/normalize.css" type="txt/css" href="css">
-        <link rel="css/style.css" type="txt/css" href="css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
 <!--        <link rel="stylesheet" href="http://basehold.it/24">-->
     </head>
     <body>
-        <div class="wrap">
-            <header class="main_heder">
-                <div class="logo_wrap"></div>
-                <div class="login"></div>
-                <div class="top_nav"></div>
+        <div class="wrap">            
+            <header class="main_heder">               
+                <a href="home.php"><img class="logo" src="img/logo.png" alt="logo"></a>            
             </header>
-            <div class="content_wrap"></div>
-            <footer class="main_footer"></footer>
+            <div class="login">
+                <div class="input-group margin-bottom-sm">
+                    <span class="input-group-addon"><i class="fa fa-envelope-o fa-fw"></i></span>
+                    <input class="form-control" type="text" placeholder="Email adresa">
+                </div>
+                <div class="input-group">
+                    <span class="input-group-addon"><i class="fa fa-key fa-fw"></i></span>
+                    <input class="form-control" type="password" placeholder="Lozinka">
+                </div>      
+                <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">Sign in</button>
+            </div>
+            <footer class="main_footer">
+                <p>Sva prava zadržana  2014 &copy RemidME</p>               
+            </footer>
         </div>
+        
+        </div>
+        <script src="js/jquery.js"></script>        
+        <script src="js/bootstrap.js"></script>
+        <script src="js/fontawesome.js"></script>
+
+
     </body>
 </html>
